91878c15fc56d06dda0db668c30fb340e3cd9e1a
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-25 02:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function toCase(text) {
+
   // write your code here
+}
+function toCase(text) {
+  let u = text.toUpperCase();
+  
+  // Convert text to lowercase
+  let l = text.toLowerCase();
+
+  // Return the combined string
+  return `${l}-${u}`;
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
+
